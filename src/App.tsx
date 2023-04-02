80f273fd536c25181338792485d8701060ca81ed
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-02 17:16:17 -0400

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="App">amongus</div>;
+  return (
+    <div className='mt-[5rem]'>
+      <p>Laurel Heights Electric Vehicle Club</p>    
+      <h1 className='text-6xl'>Building a sustainable future.</h1>    
+      <h3>among</h3>    
+      <p>among</p>
+    </div>
+  );
 }
 
 export default App;
