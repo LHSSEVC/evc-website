60105e945cce1fcfbd043f357e3b7cec3737473f
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-02 17:40:21 -0400

    feat: rough landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 function App() {
   return (
-    <div className='mt-[5rem]'>
-      <p>Laurel Heights Electric Vehicle Club</p>    
-      <h1 className='text-6xl'>Building a sustainable future.</h1>    
-      <h3>among</h3>    
-      <p>among</p>
+    <div className='mt-[10rem] px-[5rem] flex'>
+      <div className='w-4/6'>
+        <h2 className='text-dimmed font-normal'>Laurel Heights Electric Vehicle Club</h2>    
+        <h1 className='text-6xl'>Building for a sustainable future.</h1>    
+        <p>among</p>
+      </div>
+      <div className='flex items-center justify-center w-2/6'>
+        <p>car goes here</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dimmed: '#878593'
+      }
+    },
   },
   plugins: [],
 }
