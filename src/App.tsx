a1ed2fc0a1377f5df504e26d3cff01988e0ec2fc
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-02 20:57:16 -0400

    fix: some responsive stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 function App() {
   return (
-    <div className='mt-[10rem] px-[5rem] flex'>
-      <div className='w-4/6'>
+    <div className='mt-[10rem] flex px-[5rem]'>
+      <div className='w-[60%]'>
         <h2 className='text-dimmed font-normal'>Laurel Heights Electric Vehicle Club</h2>    
         <h1 className='text-6xl'>Building for a sustainable future.</h1>    
         <p>among</p>
       </div>
-      <div className='flex items-center justify-center w-2/6'>
+      <div className='flex items-center justify-center w-[40%]'>
         <p>car goes here</p>
       </div>
     </div>
