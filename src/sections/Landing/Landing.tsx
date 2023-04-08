ed286379eff3197160fbc24beae66ce319877b1b
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-04-07 21:29:05 -0400

    feat: rework landing page

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -1,14 +1,12 @@
-import "./Landing.scss";
-
 const Landing = () => {
     return (
-        <div className="mt-[10rem] flex px-[5rem] h-[95vh] Landing">
-            <div className="w-[60%]">
-                <h2 className="text-dimmed font-normal">
+        <div className="Landing flex px-[5rem] h-[95vh] Landing">
+            <div className="w-[60%] mt-[10rem]">
+                <h2 className="text-dimmed text-2xl mb-4">
                     Laurel Heights Electric Vehicle Club
                 </h2>
-                <h1 className="text-6xl">Building for a sustainable future.</h1>
-                <p>The Electric Vehicle Club believes in a </p>
+                <h1 className="text-6xl mb-12 font-bold">Building for a <span className='gradient'>sustainable</span> future.</h1>
+                <p className='text-dimmed text-xl'>Join a team of creative and curious students who are passionate about pushing the boundaries of what's possible in electric vehicle design and racing.</p>
             </div>
             <div className="flex items-center justify-center w-[40%]">
                 <p>car goes here</p>
