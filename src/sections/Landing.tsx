476e85d0924a305410bc79eb278abd40115a2b0a
Daniel Zhang <d.zhang200788@gmail.com>
2023-04-11 14:28:29 -0400

    smooth scrolling but it doesn't work and upload prospectus

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,4 +1,4 @@
-import world from '../assets/world.png'
+import world from "../assets/world.png";
 
 const Landing = () => {
     return (
@@ -7,9 +7,25 @@ const Landing = () => {
                 <h2 className="text-dimmed text-2xl mb-4">
                     Laurel Heights Electric Vehicle Club
                 </h2>
-                <h1 className="text-6xl mb-8 font-bold">Building for a <span className='gradient'>sustainable</span> future.</h1>
-                <p className='text-dimmed text-xl'>Join a team of creative and curious students who are passionate about pushing the boundaries of what's possible in electric vehicle design and racing.</p>
-                <button onClick={() => document.getElementById('about')?.scrollIntoView()} className='mt-4 bg-gradient-to-r from-cyan-500 to-blue-500 hover:brightness-90'>Learn More</button>
+                <h1 className="text-6xl mb-8 font-bold">
+                    Building for a <span className="gradient">sustainable</span>{" "}
+                    future.
+                </h1>
+                <p className="text-dimmed text-xl">
+                    Join a team of creative and curious students who are
+                    passionate about pushing the boundaries of what's possible
+                    in electric vehicle design and racing.
+                </p>
+                <button
+                    onClick={() =>
+                        document
+                            .getElementById("about")
+                            ?.scrollIntoView({ behavior: "smooth" })
+                    }
+                    className="mt-4 bg-gradient-to-r from-cyan-500 to-blue-500 hover:brightness-90"
+                >
+                    Learn More
+                </button>
             </div>
             <div className="flex items-center justify-center w-[50%]">
                 <a>car goes here</a>
